f50ec9d7bbdfaa8e612265a23cdfcd890d821a86
zsj9418zsj <140706155+zsj9418zsj@users.noreply.github.com>
2024-11-23 20:05:50 +0800

    Update diy-part2-6-1.sh

--- diy-part2-6-1.sh
+++ diy-part2-6-1.sh
@@ -11,13 +11,15 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.60/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt-2.4G/cmcc/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ImmortalWrt-5G/cmcc_5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/ImmortalWrt/ImmortalWrt-Hanwckf/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/360T7/g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-/1' include/image.mk
